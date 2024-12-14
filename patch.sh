6ec7936cf8296f10f3bb1d4c5834da37e9ef573b
LunarStorm97 <ian8388382@proton.me>
2024-12-13 23:43:49 -0300

    ...

--- patch.sh
+++ patch.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 
-# Instalar dependencias necesarias
-apt-get install -y lz4 tar openssl
-
-# Descomprimir recovery.img.lz4 si existe
 if [ -f recovery.img.lz4 ]; then
     lz4 -f recovery.img.lz4 recovery.img
     mv recovery.img.lz4 recovery.img.lz4-orig
 fi
 
-# Extraer imagen base
 off=$(grep -ab -o SEANDROIDENFORCE recovery.img | tail -n 1 | cut -d : -f 1)
 if [ -n "$off" ]; then
     dd if=recovery.img of=r.img bs=4k count="$off" iflag=count_bytes
-else
-    echo "Error: No se pudo encontrar el marcador SEANDROIDENFORCE"
-    exit 1
 fi
 
-# Generar clave RSA si no existe
 if [ ! -f phh.pem ]; then
     openssl genrsa -f4 -out phh.pem 4096
 fi
 
-# Crear directorio de trabajo
 mkdir -p unpack
 cd unpack
 
-# Desempaquetar y modificar ramdisk
 ../magiskboot unpack ../r.img
 ../magiskboot cpio ramdisk.cpio extract
 cp prop.default ../prop.default
 
-# Aplicar parches binarios
 ../magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094
 ../magiskboot hexpatch system/bin/recovery eec3009420010034 eec3009420010035
 ../magiskboot hexpatch system/bin/recovery 3ad3009420010034 3ad3009420010035
@@ -45,16 +33,13 @@ cp prop.default ../prop.default
 ../magiskboot hexpatch system/bin/recovery 24f0fcee30b1681c 24f0fcee30b9681c
 ../magiskboot hexpatch system/bin/recovery 27f02eeb30b1681c 27f02eeb30b9681c
 
-# Reagregar archivos al ramdisk
 ../magiskboot cpio ramdisk.cpio 'add 0755 system/bin/recovery system/bin/recovery'
 ../magiskboot cpio ramdisk.cpio 'add 0755 prop.default prop.default'
 
-# Reempaquetar imagen
 ../magiskboot repack ../r.img new-boot.img
 cp new-boot.img ../recovery-patched.img
 cd ..
 
-# Comprimir y empaquetar archivos finales
 if [ -f recovery-patched.img ]; then
     mv recovery-patched.img recovery.img
     lz4 -B6 --content-size recovery.img recovery.img.lz4
@@ -63,6 +48,5 @@ if [ -f recovery-patched.img ]; then
     mv patched-recovery.tar patched-recovery.tar.md5
 fi
 
-# Limpiar archivos temporales
 rm -f r.img recovery.img recovery.img.lz4 phh.pem prop.default
 rm -rf unpack
